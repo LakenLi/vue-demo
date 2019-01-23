8715400823ee87136bdb99c9361a9d298e7978e6
lihaoran <lihaoran@xiaoyouzi.com>
2019-01-23 17:37:01 +0800

    webpack学习

--- vue_demo/vue_todo_app/src/index.js
+++ vue_demo/vue_todo_app/src/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './app.vue'
 
+import './assets/styles/test.css'
+import './assets/images/bg.jpg'
+import './assets/styles/test.stylus.styl'
+
 const root = document.createElemen('div')
 document.body.appendChild(root)
 
--- vue_demo/vue_todo_app/webpack.config.js
+++ vue_demo/vue_todo_app/webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const UrlLoaderPlugin = require('url-loader')
+//const CssLoaderPlugin = require('css-loader')
 
 module.exports = {
     entry: path.join(__dirname, 'src/index.js'),
@@ -8,14 +10,43 @@ module.exports = {
         path: path.join(__dirname, 'dist')
     },
     plugins:[
-        new VueLoaderPlugin()
+        new VueLoaderPlugin(),
+        //new CssLoaderPlugin(),
+        //new UrlLoaderPlugin(),
     ],
     module: {
         rules: [
             {
-                test: /.vue$/,
-                loader: 'vue-loader'
-            }
+                test: /\.vue$/,
+                loader: ['vue-loader', 'style-loader']
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.styl$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'stylus-loader'
+                ]
+            },
+            {
+                test: /\.(gif|jpg|png|jpeg|svg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options:{
+                            limit: 1024,
+                            name: '[name].[ext]'
+                        }
+                    }
+                ]
+            },
         ]
     }
 }
